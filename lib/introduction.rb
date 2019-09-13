3481d60153785d0a8a9a417262292329b652748d
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-13 14:45:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I'm learning to program in #{language}."
+def introduction_with_language
 end
-introduction_with_language("Dan", "Ember.js")
